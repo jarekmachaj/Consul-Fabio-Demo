96a9086fae778d2bde0d695d81b53a7e5c0d0dae
Jarek Machaj <jarekmachaj@users.noreply.github.com>
2019-12-27 15:37:49 +0100

    newsett

---
+++ webapps/webApp2/ConsulSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApp2
+{
+    public class ConsulSettings
+    {
+        public string ConsulAddress { get; set; }
+        public string Token { get; set; }
+    }
+}
--- webapps/webApp2/Startup.cs
+++ webapps/webApp2/Startup.cs
@@ -11,96 +11,52 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace webApp2
 {
     public class Startup
     {
         private readonly ILogger _logger;
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration _configuration { get; set; }
 
-        public Startup(ILogger<Startup> logger)
+        public Startup(ILogger<Startup> logger, IConfiguration configuration)
         {
              _logger = logger;
+            _configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
             _logger.LogInformation($"Total Services Initially: {services.Count}");
+            services.Configure<DatabaseSettings>(_configuration.GetSection("DatabaseSettings"));
+            services.Configure<ConsulSettings>(_configuration.GetSection(nameof(ConsulSettings)));
+
+            services.ConfigureAll<DatabaseSettings>(opt =>
+            {
+                opt.ConnectionString = "xxx";
+            });
+
+
+            var consulSettings = _configuration.GetSection(nameof(ConsulSettings)).Get<ConsulSettings>();
+            services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
+            {
+                consulConfig.Address = new Uri(consulSettings.ConsulAddress);
+                consulConfig.Token = consulSettings.Token;
+            }));           
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseRouting();
-
-            var client = new ConsulClient();
-
-             if (app.Properties["server.Features"] is FeatureCollection features)
-             {
-                var addresses = features.Get<IServerAddressesFeature>();                 
-                var address = addresses.Addresses.First();
-                _logger.LogInformation($"Adress: {address}");
-
-                var serviceId = Guid.NewGuid().ToString();
-                var uri = new Uri(address);
-                var entryAssemblyName = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
-                var agentReg = new AgentServiceRegistration()
-                {
-                    Address = "host.docker.internal",//uri.Host,
-                    ID = serviceId,
-                    Name = entryAssemblyName,
-                    Port = uri.Port,
-                    Check = new AgentServiceCheck
-                    {
-                        Interval = TimeSpan.FromSeconds(5),
-                        DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),
-                        HTTP = $"http://host.docker.internal:{uri.Port}/healthcheck"
-                    },
-                    Tags = new[] { "urlprefix-/app/val" }
-                };
-        
-                client.Agent.ServiceRegister(agentReg).GetAwaiter().GetResult();
-            }
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-
-                endpoints.MapGet("/new", async context => {
-                    await context.Response.WriteAsync("new from app2");
-                });
-
-                endpoints.MapGet("/healthcheck", async context => {
-                    await context.Response.WriteAsync("OK");
-                });
-
-                endpoints.MapGet("/app/val", async context => {
-                    await context.Response.WriteAsync("OK from app2");
-                });              
-
-                endpoints.MapGet("/app/val/set", async context => {
-                    var putPair = new KVPair("connectionString")
-                    {
-                        Value = Encoding.UTF8.GetBytes("TakiConnString")
-                    };
-                    var putAttempt = await client.KV.Put(putPair);
-
-                    var getPair = await client.KV.Get("apps/jpc/settings/devices");
-                    await context.Response.WriteAsync(Encoding.UTF8.GetString(getPair.Response.Value, 0, getPair.Response.Value.Length));
-                });  
-
-                
-            });
+            app.UseConsul();
+            app.UseDemoEndpoints();            
         }
     }
 }
